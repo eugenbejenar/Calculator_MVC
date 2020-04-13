288b34a511149362aa21b2d6d792694f3399dc90
eugenbejenar <jekabejenar@gmail.com>
2020-04-13 13:24:30 -0700

    v1

---
+++ Calculator/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using Calculator.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Calculator.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET
+        public IActionResult Index(Operations calc )
+        {
+            return View(calc);
+        }
+        
+        
+        [HttpPost]
+        public IActionResult Calculate(Operations calc)
+        {
+            calc.result = CalculateResult(calc);
+
+            return RedirectToAction("Index", calc);
+
+
+        }
+
+        private double CalculateResult(Operations calc)
+        {
+            double result = 0;
+            switch (calc.operation)
+            {
+                case "+":
+                {
+                    result = calc.firstNum + calc.secondNum;
+                }
+                    break;
+                case "-":
+                {
+                    result = calc.firstNum - calc.secondNum;
+                }
+                    break;
+                case "*":
+                {
+                    result = calc.firstNum * calc.secondNum;
+                }
+                    break;
+                case "/":
+                {
+                    result = calc.firstNum / calc.secondNum;
+                }
+                    break;
+
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Calculator/Models/Operations.cs
@@ -0,0 +1,10 @@
+﻿namespace Calculator.Models
+{
+    public class Operations
+    {
+        public double firstNum { get; set; }
+        public double secondNum { get; set; }
+        public string operation { get; set; }
+        public double result { get; set; }
+    }
+}
